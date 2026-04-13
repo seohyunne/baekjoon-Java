fc31a505b6825193c79164fdcccfb037e7b3b269
seohyunne <112487626+seohyunne@users.noreply.github.com>
2026-04-13 15:25:43 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16620 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,44 +1,41 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for(int i=0; i<N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A); // 오름차순 정렬
+
+        // arr 배열 오름차순 정렬
+        Arrays.sort(arr);
 
         int count = 0;
-        // 투 포인터 (i,j) 
-        int i=0; // A[0]
-        int j=N-1; // A[N-1]
+        int i=0;
+        int j=N-1;
 
         while(i<j){
-            if(A[i]+A[j]<M){
-                i++;
-            }
-            else if (A[i]+A[j]>M){
+            if(arr[i]+arr[j]>M){
                 j--;
             }
-            else{
-                count++;
+            else if(arr[i]+arr[j]<M){
                 i++;
-                j--;
+            }
+            else if(arr[i]+arr[j]==M){
+                i++; j--; count++;
             }
         }
         System.out.println(count);
+        br.close();
     }
 }
