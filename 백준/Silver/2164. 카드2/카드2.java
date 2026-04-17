7e63db1348b134f75125b63f4dce6fefaba1d84f
seohyunne <112487626+seohyunne@users.noreply.github.com>
2026-04-17 16:30:55 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 49076 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
         Queue<Integer> queue = new LinkedList<>();
+        
+        int n = input.nextInt();
 
-        // 큐 생성
-        for(int i=0;i<N;i++){
-            queue.add(i+1);
+        for(int i=1; i<n+1; i++){
+            queue.add(i);
         }
 
         while(queue.size()>1){
-            // 최상단 수 제거
-            queue.remove();
-            int top = queue.poll();
-            queue.add(top);
+            queue.poll();
+            queue.add(queue.poll());
         }
+        System.out.println(queue.poll());
 
-        System.out.println(queue.peek());
     }
 }
